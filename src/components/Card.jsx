d79f23266bbb0e3ae65b205cd474b017af39278c
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-06-26 14:24:27 -0500

    Add hover effect to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,33 @@
 import "../styles/Card.css";
+import image from "../images/ragnaros.png";
 
 function Card() {
-  return <div className="card">Card</div>;
+  const handleMouseMove = (e) => {
+    const image = e.currentTarget.querySelector(".cardImage");
+    const { top, left, width, height } =
+      e.currentTarget.getBoundingClientRect();
+    const centerX = left + width / 2;
+    const centerY = top + height / 2;
+
+    const rotateY = ((centerX - e.clientX) / (width / 2)) * 10;
+    const rotateX = ((e.clientY - centerY) / (height / 2)) * 20;
+    image.style.transform = `perspective(500px) translateZ(50px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  };
+
+  const handleMouseLeave = (e) => {
+    const image = e.currentTarget.querySelector(".cardImage");
+    image.style.transform = "unset";
+  };
+
+  return (
+    <button
+      className="card"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img className="cardImage" src={image} alt="" />
+    </button>
+  );
 }
 
 export default Card;
