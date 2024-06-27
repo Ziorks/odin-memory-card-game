a392ed74f72f16ad435bcb54379d71e6e2f36a02
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-06-27 14:35:56 -0500

    Add api calls (to fix: round effect should include game over/ get new card if no image)

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,29 @@
 import Card from "./Card";
 import Scoreboard from "./Scoreboard";
 import "../styles/Game.css";
+import { useEffect, useRef, useState } from "react";
 
-import image1 from "../images/ragnaros.png";
-import image2 from "../images/angryChicken.png";
-import image3 from "../images/youthfulBrewmaster.png";
-import image4 from "../images/chillwindYeti.png";
-import { useRef, useState } from "react";
+async function getAllCards() {
+  const url =
+    "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/classic";
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "17518acd8cmsh26198022283e5b0p1f5363jsn979e85c40785",
+      "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
 
-const shuffleArray = (array) => {
+  try {
+    const resp = await fetch(url, options);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function shuffleArray(array) {
   let currentIndex = array.length;
 
   while (currentIndex != 0) {
@@ -20,55 +35,85 @@ const shuffleArray = (array) => {
       array[currentIndex],
     ];
   }
-};
-
-const generateCardData = (nCards) => {
-  //replace this with api call
-  const testData = [
-    {
-      id: 0,
-      clicked: false,
-      image: image1,
-      description: "Ragnaros the Firelord",
-    },
-    {
-      id: 1,
-      clicked: false,
-      image: image2,
-      description: "Angry Chicken",
-    },
-    {
-      id: 2,
-      clicked: false,
-      image: image3,
-      description: "Youthful Brewmaster",
-    },
-    {
-      id: 3,
-      clicked: false,
-      image: image4,
-      description: "Chillwind Yeti",
-    },
-  ];
-
-  const result = [];
-  for (let i = 0; i < nCards; i++) {
-    result.push(testData[Math.floor(Math.random() * testData.length)]);
-  }
-  shuffleArray(result);
-  return result;
-};
+}
 
 function Game() {
-  const [round, setRound] = useState(1);
-  const [cardData, setCardData] = useState(generateCardData(4));
+  const [loading, setLoading] = useState(true);
+  const [round, setRound] = useState(0);
+  const [cardData, setCardData] = useState([]);
   const highScore = useRef(0);
+  const cardPool = useRef(null);
 
   const prevRound = round - 1;
-  const prevRoundsScore = prevRound * 2 + (prevRound / 2) * (2 + prevRound * 2);
+  const prevRoundsScore =
+    round > 0 ? prevRound * 2 + (prevRound / 2) * (2 + prevRound * 2) : 0;
   const currentRoundScore = cardData.filter(({ clicked }) => clicked).length;
   const score = prevRoundsScore + currentRoundScore;
   highScore.current = Math.max(highScore.current, score);
+
+  useEffect(() => {
+    let ignore = false;
+
+    getAllCards().then((result) => {
+      if (!ignore) {
+        cardPool.current = result;
+        setRound(1);
+        console.log("Card Pool Received!");
+      }
+    });
+
+    return () => (ignore = true);
+  }, []);
+
+  useEffect(() => {
+    const promises = [];
+    const nCards = round > 0 ? 2 + round * 2 : 0;
+    const cardIds = new Set();
+
+    while (cardIds.size < nCards) {
+      const randomCardIndex = Math.floor(
+        Math.random() * cardPool.current.length
+      );
+      const randomCardId = cardPool.current[randomCardIndex].cardId;
+      cardIds.add(randomCardId);
+    }
+
+    cardIds.forEach((id) => {
+      const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" + id;
+      const options = {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "17518acd8cmsh26198022283e5b0p1f5363jsn979e85c40785",
+          "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+        },
+      };
+
+      const promise = new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then((resp) => resp.json())
+          .then((data) => {
+            console.log(data);
+            const cardData = {
+              id,
+              clicked: false,
+              image: data[0].img,
+              description: data[0].name,
+            };
+            resolve(cardData);
+          })
+          .catch((error) => reject(error));
+      });
+
+      promises.push(promise);
+    });
+
+    Promise.all(promises).then((cards) => {
+      console.log(cards);
+      setCardData(cards);
+      setLoading(false);
+    });
+  }, [round]);
 
   const handleClick = (id) => {
     const clickedCardIndex = cardData.findIndex((card) => card.id === id);
@@ -76,16 +121,15 @@ function Game() {
     //if clicked a card that has already been clicked
     if (cardData[clickedCardIndex].clicked) {
       setRound(1);
-      setCardData(generateCardData(4));
+      setLoading(true);
       return;
     }
 
     //if clicked last unclicked card of round
     if (currentRoundScore + 1 === cardData.length) {
-      const nextRound = round + 1;
-      const nNewCards = 2 + nextRound * 2;
-      setRound(nextRound);
-      setCardData(generateCardData(nNewCards));
+      setRound(round + 1);
+      setCardData([]);
+      setLoading(true);
       return;
     }
 
@@ -98,17 +142,25 @@ function Game() {
 
   return (
     <main className="game">
-      <Scoreboard currentScore={score} highScore={highScore.current} />
-      <div className="cardsContainer">
-        {cardData.map(({ id, image, description }) => (
-          <Card
-            key={id}
-            image={image}
-            description={description}
-            onClick={() => handleClick(id)}
-          />
-        ))}
-      </div>
+      <Scoreboard
+        currentScore={score}
+        highScore={highScore.current}
+        round={round}
+      />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="cardsContainer">
+          {cardData.map(({ id, image, description }) => (
+            <Card
+              key={id}
+              image={image}
+              description={description}
+              onClick={() => handleClick(id)}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,9 +1,10 @@
 import "../styles/Scoreboard.css";
 
-function Scoreboard({ currentScore, highScore }) {
+function Scoreboard({ currentScore, highScore, round }) {
   return (
     <div className="scoreboard">
       <h2>High Score: {highScore}</h2>
+      <h2>Round: {round}</h2>
       <h2>Score: {currentScore}</h2>
     </div>
   );
